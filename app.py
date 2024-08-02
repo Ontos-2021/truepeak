9c11c1177e3c66e3eddb6f5855c23ec00f888842
Ontos <josemiguel.mercado17@gmail.com>
2024-08-02 10:15:08 -0300

    La app funciona perfectamente!

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import os
 from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
-import scipy.io.wavfile as wavfile
+import librosa
 import numpy as np
-from pydub import AudioSegment
+import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'tmp'  # Configura aquí la carpeta de uploads
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,39 +13,34 @@ def index():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])  # Crea la carpeta si no existe
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filepath = os.path.join('tmp', filename)
             file.save(filepath)
-            if filename.endswith('.mp3'):
-                audio_path = convert_to_wav(filepath)
-            else:
-                audio_path = filepath
-            true_peak_dbfs = analyze_true_peak(audio_path)
-            os.remove(filepath)  # Limpieza del archivo almacenado
-            if filename.endswith('.mp3'):
-                os.remove(audio_path)  # Limpieza del archivo WAV convertido
+            true_peak_dbfs = analyze_true_peak(filepath)
+            os.remove(filepath)  # Cleanup the stored file
             return render_template('index.html', result=f"The true peak of the audio file is: {true_peak_dbfs} dBFS")
     return render_template('index.html', result=None)
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'wav', 'mp3'}
 
-def convert_to_wav(mp3_path):
-    wav_path = mp3_path.rsplit('.', 1)[0] + '.wav'
-    audio = AudioSegment.from_file(mp3_path)
-    audio.export(wav_path, format='wav')
-    return wav_path
 
-def analyze_true_peak(wav_path):
-    rate, audio_data = wavfile.read(wav_path)
-    if audio_data.dtype == np.int16:
-        audio_data = audio_data.astype(np.float32) / np.iinfo(np.int16).max
-    elif audio_data.dtype == np.int32:
-        audio_data = audio_data.astype(np.float32) / np.iinfo(np.int32).max
+def analyze_true_peak(file_path):
+    # Cargar archivo de audio
+    audio_data, rate = librosa.load(file_path, sr=None)  # Cargar con su sample rate original
+
+    # Extraer el canal de audio si es estéreo
+    if audio_data.ndim > 1:
+        audio_data = audio_data.mean(axis=1)
+
+    # Calcular el true peak
     true_peak = np.max(np.abs(audio_data))
+
+    # Convertir a dBFS
     true_peak_dbfs = 20 * np.log10(true_peak)
+
     return true_peak_dbfs
+
 
 if __name__ == '__main__':
     app.run(debug=True)
