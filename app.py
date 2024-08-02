0a17cc1ce554efc95c576db46b4ce54bec011974
Ontos <josemiguel.mercado17@gmail.com>
2024-08-02 10:50:32 -0300

    Ahora da dbfs, rms y loudness integrado

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 import librosa
 import numpy as np
+import pyloudnorm as pyln
 import os
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,23 +18,37 @@ def index():
                 filename = secure_filename(file.filename)
                 filepath = os.path.join('tmp', filename)
                 file.save(filepath)
-                true_peak_dbfs = analyze_true_peak(filepath)
+                analysis_results = analyze_audio(filepath)
                 os.remove(filepath)  # Cleanup the stored file
-                results.append((filename, true_peak_dbfs))
+                results.append((filename, analysis_results))
         return render_template('index.html', results=results)
     return render_template('index.html', results=None)
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'wav', 'mp3'}
 
-def analyze_true_peak(file_path):
+
+def analyze_audio(file_path):
     # Cargar archivo de audio
     audio_data, rate = librosa.load(file_path, sr=None)  # Cargar con su sample rate original
     if audio_data.ndim > 1:
         audio_data = audio_data.mean(axis=1)
+
+    # Calcular True Peak
     true_peak = np.max(np.abs(audio_data))
     true_peak_dbfs = 20 * np.log10(true_peak)
-    return true_peak_dbfs
+
+    # Calcular RMS
+    rms_value = np.sqrt(np.mean(np.square(audio_data)))
+    rms_db = 20 * np.log10(rms_value)
+
+    # Calcular Loudness
+    meter = pyln.Meter(rate)  # crear un medidor de sonoridad
+    loudness_integrated = meter.integrated_loudness(audio_data)
+
+    return true_peak_dbfs, rms_db, loudness_integrated
+
 
 if __name__ == '__main__':
     app.run(debug=True)
