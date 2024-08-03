910d623e7886fd284c4e846f2168c2697e922191
Ontos <josemiguel.mercado17@gmail.com>
2024-08-02 21:54:29 -0300

    Ahora arroja el loudness momentary y short term

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -24,10 +23,8 @@ def index():
         return render_template('index.html', results=results)
     return render_template('index.html', results=None)
 
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'wav', 'mp3'}
-
 
 def analyze_audio(file_path):
     # Cargar archivo de audio
@@ -43,12 +40,33 @@ def analyze_audio(file_path):
     rms_value = np.sqrt(np.mean(np.square(audio_data)))
     rms_db = 20 * np.log10(rms_value)
 
-    # Calcular Loudness
-    meter = pyln.Meter(rate)  # crear un medidor de sonoridad
+    # Crear un medidor de sonoridad
+    meter = pyln.Meter(rate)
+
+    # Calcular Loudness Integrado
     loudness_integrated = meter.integrated_loudness(audio_data)
 
-    return true_peak_dbfs, rms_db, loudness_integrated
+    # Calcular Loudness Momentáneo y su valor máximo
+    block_size_momentary = int(0.4 * rate)
+    momentary_loudness = [meter.integrated_loudness(audio_data[i:i + block_size_momentary])
+                          for i in range(0, len(audio_data), block_size_momentary)
+                          if len(audio_data[i:i + block_size_momentary]) == block_size_momentary]
+    max_momentary_loudness = np.max(momentary_loudness) if momentary_loudness else float('nan')
 
+    # Calcular Loudness a Corto Plazo y su valor máximo
+    block_size_short_term = int(3 * rate)
+    short_term_loudness = [meter.integrated_loudness(audio_data[i:i + block_size_short_term])
+                           for i in range(0, len(audio_data), block_size_short_term)
+                           if len(audio_data[i:i + block_size_short_term]) == block_size_short_term]
+    max_short_term_loudness = np.max(short_term_loudness) if short_term_loudness else float('nan')
+
+    return {
+        "true_peak_dbfs": true_peak_dbfs,
+        "rms_db": rms_db,
+        "loudness_integrated": loudness_integrated,
+        "max_momentary_loudness": max_momentary_loudness,
+        "max_short_term_loudness": max_short_term_loudness
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
